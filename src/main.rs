e6b74c5c243885b8a4d2e44603ecb40aa760f63c
Akash-YS05 <akash.pan05@gmail.com>
2025-02-16 11:44:54 +0530

    data types

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let arr: [i8; 5] = [2,3,4,5,6];    //cant grow, size needs to be known at compile time
+    let mut vect: Vec<i8> = vec![6,7,8,9,10]; //can grow, size can be changed at runtime
+    vect.push(11);
+    let word = String::from("Hello, World! ");
+    let isx: bool = false;
+    let x: i32 = 5;
+    let mut y: i32 = 10;
+    y+=10;
+    println!("{}", word);
+    println!("Value of x (unmutable) is: {}", x);
+    println!("Value of y (mutable) is: {}", y+10);
+    println!("isx current status is {}", isx);
+    println!("Array is : {:?}", arr);
+    println!("Vector is : {:?}", vect);
+    println!("Length of array is: {}", arr.len());
+   
+}
