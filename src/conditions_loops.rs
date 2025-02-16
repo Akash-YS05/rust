4e8049d43ce8353126c7db07715d4a85fe9754aa
Akash-YS05 <akash.pan05@gmail.com>
2025-02-16 12:20:23 +0530

    loop

--- src/conditions_loops.rs
+++ src/conditions_loops.rs
@@ -7,8 +7,27 @@ fn main() {
         println!("Number is odd");
     }
 
+    pattern(5);
+
+
+    //equivalent code for (int i = 0; i<10; i+=2)
+    for z in (0..10).step_by(2) {
+        println!("{}", z);
+    }
+
 
     pub fn is_even(x: i8) -> bool {
         return x % 2 == 0;
+    }
+
+
+    //no arrow after data type means it returns void
+    pub fn pattern(x: i8) {
+        for i in 1..x+1 {
+            for _j in 0..i {
+                print!("*");
+            }
+            println!();
+        }
     }
 }
