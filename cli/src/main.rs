5fee08315451803454469e110fb7e8d6f4e431fe
Akash-YS05 <akash.pan05@gmail.com>
2025-02-20 01:18:23 +0530

    add fn on int args

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,10 +3,20 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //args[0] is the name of program
-    let c1 = &args[1];
-    let c2 = &args[2];
+    // //args[0] is the name of program
+    // let c1 = &args[1];
+    // let c2 = &args[2];
+
+    // println!("First argument is {c1}");
+    // println!("Second argument is {c2}");
+
+    let c1: i32 = args[1].parse().expect("Please provide a valid integer for the first argument");
+    let c2: i32 = args[2].parse().expect("Please provide a valid integer for the second argument");
+
+    let sum: i32 = c1 + c2;
 
     println!("First argument is {c1}");
-    println!("Second argument is {c2}")
+    println!("Second argument is {c2}");
+    println!("Sum = {sum}");
+
 }
