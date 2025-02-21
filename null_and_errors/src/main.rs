96d9d0a52357ba51c67146ac37a141aefabf11f0
Akash-YS05 <akash.pan05@gmail.com>
2025-02-22 01:25:47 +0530

    null value handling using Option

--- null_and_errors/src/main.rs
+++ null_and_errors/src/main.rs
@@ -1,12 +1,31 @@
 //Error handling is done by Result which is an enum with two variants - Ok and Err
-
+//option enum is used to handle null values as Rust does not have null variable
 use std::fs::File;
 
 fn main() {
-    let test_file = File::open("hello.txt");
 
-    let file = match test_file {
-        Ok(file) => file,
-        Err(error) => panic!("Some error occured! {error:?}")
-    };
+    let name = find_first(String::from("monaco"));
+
+    match name {
+        Some(value) => println!("Index is {value}"),
+        None => println!("No value found")
+    }
+    // let test_file = File::open("hello.txt");
+
+    // let file = match test_file {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Some error occured! {error:?}")
+    // };
+
+
+}
+
+fn find_first(name: String) -> Option<i32> {
+    for (index, char) in name.chars().enumerate() {
+        if char == 'a' {
+            return Some(index as i32);    //if the output is desirable then use Some variant of Option enum
+        } 
+    }
+    return None;    //if the output is not desirable then use None variant of Option enum
+
 }
